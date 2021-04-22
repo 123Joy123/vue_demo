a7dfe4043a920d34dd949e2c93f0641130d1cc1e
123Joy123 <2506114287@qq.com>
2021-04-22 21:52:30 +0800

    完成基本页面和删除功能

---
+++ src/components/Botton.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="onClick()" :style="{backgroundColor:color}" class="btn">{{title}}</button>
+</template>
+
+<script>
+export default {
+    name:'Button',
+    props:{
+        title:String,
+        color:String,
+    },
+    methods:{
+        onClick(){
+            console.log("click");
+        },
+    },
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button title="Add Task" color="green"/>
+    </header>
+</template>
+
+<script>
+import Button from './Botton'
+
+export default {
+    name:'Header',
+    props:{
+        title:String,
+    },
+    components:{
+        Button,
+    },
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>>
